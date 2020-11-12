4b6b30c439a0f7a761291895fd4966f5e0594dad
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-11-12 06:32:55 +0100

    chore: update to Angular 11

--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
 
@@ -6,7 +6,7 @@ describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BookComponent ]
     })
--- src/app/secure/secure-content/secure-content.component.spec.ts
+++ src/app/secure/secure-content/secure-content.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SecureContentComponent } from './secure-content.component';
 
@@ -6,7 +6,7 @@ describe('SecureContentComponent', () => {
   let component: SecureContentComponent;
   let fixture: ComponentFixture<SecureContentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SecureContentComponent ]
     })
